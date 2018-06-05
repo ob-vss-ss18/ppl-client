9354432f367448e7493527cdf077a8ccc3433388
Ömer Ergan <erganmedia@gmail.com>
2018-06-05 11:02:09 +0200

    get data via graphql

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import { NgxElectronModule } from 'ngx-electron';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
-    NgxElectronModule
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule,
+    NgxElectronModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(
+    apollo: Apollo,
+    httpLink: HttpLink
+  ) {
+    apollo.create({
+      link: httpLink.create({ uri: 'https://vm8mjvrnv3.lp.gql.zone/graphql' }),
+      cache: new InMemoryCache
+    })
+  }
+ }
